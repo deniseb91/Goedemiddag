37a4c1cec10e899e7b97db8c7f7afbea6a7d46ff
deniseb91 <102030988+deniseb91@users.noreply.github.com>
2022-11-10 10:49:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+/*
+*{
+    margin:0;
+    padding:0
+}
+
+.box1 {
+    background-color: blue;
+    padding: 2em;
+    flex:1;
+}
+
+.box2{
+    background-color: red;
+    padding: 2em;
+    flex:1;
+}
+
+.subbox2{
+    background-color: grey;
+}
+
+.container {
+    display:flex;
+    width: 100vw;
+    align-items: center;
+    justify-content: center;
+    margin:0 auto;
+    color:white;
+   
+}
+.container ul{
+list-style-type: none;
+padding: 2em;}*/
